cc50a7731f1dc3a623947d081134eb4664269220
krens patel <krenspatel17@gmail.com>
2020-02-06 17:15:31 -0500

    Completed lab4

---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+           
+            PhoneName = "";
+            
+            DateReleased = DateTime.Now ;
+        }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+        [Key]
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; }
+        public int MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+}
